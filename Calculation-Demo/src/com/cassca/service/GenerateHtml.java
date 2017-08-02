0417425c3d3e0259b817a748a851c4b274cc8f37
MonkeyFlyFly <704504641@qq.com>
2017-08-02 18:43:04 +0800

    for html and xml

--- Calculation-Demo/src/com/cassca/service/GenerateHtml.java
+++ Calculation-Demo/src/com/cassca/service/GenerateHtml.java
@@ -4,6 +4,23 @@ public class GenerateHtml {
 	private GenerateHtml(){};
 	public static String generateHtml() {
 
-		return "";
+		
+		String head_ele="<%@ page language=\"java\" contentType=\"text/html; charset=UTF-8\" pageEncoding=\"UTF-8\"%>\n"
+						+"<%@ include file=\"/WEB-INF/jsp/apex/inc/taglib.jsp\"%>\n"
+						+"<script src=\"${appRoot}/static/ret/js/retCommon.js\" type=\"text/javascript\"></script>\n"
+						+"<link href=\"${appRoot}/static/ret/css/returnfirst_en_us.css\" type=\"text/css\" rel=\"stylesheet\" />\n";
+
+		
+		String fist_ele="<div style=\"text-align: center;\">"
+						+"<img src=\"${appRoot}/static/ret/images/image.gif\" />"
+						+"<p style=\"font-size: 20px;\">"
+						+"<spring:message code=\"com.cacss.itas.ret.returnSDR.StampDuty\" text=\"Stamp Duty (Agency)\" />"
+						+"</p>"
+						+"</div>";
+						
+		return head_ele;
+	}
+	public static void main(String[] args) {
+		System.out.println(generateHtml());
 	}
 }
--- Calculation-Demo/src/com/cassca/service/GenerateXml.java
+++ Calculation-Demo/src/com/cassca/service/GenerateXml.java
@@ -1,10 +1,38 @@
 package com.cassca.service;
 
+
 public class GenerateXml {
 	private GenerateXml(){};
-	public static String generateXml(String prefix,String...fields) {
-
-		return "";
+	public static String generateXml(String prefix,String returnShortName,String returnName,String[] fields,String[] desc) {
+		String head_ele="<?xml version=\"1.0\"?>\n"+
+						"<Fields>\n";
+		String last_ele="</Fields>";
+		
+		
+		StringBuilder body_ele=new StringBuilder();
+		String ele_type="DOUBLE";
+		String isTable="false";
+		String isRequired="false";
+		for (int i = 0; i < fields.length; i++) {
+			body_ele.append("<RetFormRule>\n"
+			+"<code>"+prefix+fields[i]+"</code>\n"
+			+"<parentCode/>\n"
+			+"<fieldType>"+ele_type+"</fieldType>\n"
+			+"<isTable>"+isTable+"</isTable>\n"
+			+"<isRequired>"+isRequired+"</isRequired>\n"
+			+"<tag>"+prefix+fields[i]+"</tag>\n"
+			+"<desc>"+desc[i]+"</desc>\n"
+			+"<section>com.cacss.itas.ret.return"+returnShortName+"."+returnName+"</section>\n"
+			+"<i18ncode>com.cacss.itas.ret."+returnShortName+"."+fields[i]+"</i18ncode>\n"
+			+"</RetFormRule>\n");
+		}
+		
+		return head_ele+body_ele+last_ele;
+	}
+	public static void main(String[] args) {
+		String[] arr1={"S1F1","S1F2"};
+		String[] arr2={"Document Type","Quantity"};
+		System.out.println(generateXml("Cret00SDR", "SDR", "StampDuty",arr1,arr2));
 	}
 
 }
